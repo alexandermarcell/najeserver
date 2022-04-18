7ae5c68bc4b2b2e7ed787278a2a24d7022ecf178
alexandrmarcell <alexandermarcell36@gmail.com>
2022-04-18 16:44:22 -0400

    worked on stripe intergration

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const Cart = require('../models/cartModel');
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
+// const handleOrder = async() => {
+//   Cart.findOneAndDelete({ id })
+//   .then(() => {
+//     console.log('order placed!');
+//     next();
+//   })
+//   .catch(error => {
+//     console.log(`error deleting order by ID: ${error.message}`)
+//     next();
+//   })
+// }
 
-router.post("/payment", (req, res) => {
+router.post('/payment', (req, res) => {
   stripe.charges.create(
     {
       source: req.body.tokenId,
@@ -15,6 +27,7 @@ router.post("/payment", (req, res) => {
         res.status(500).json(stripeErr);
       } else {
         res.status(200).json(stripeRes);
+        console.log(stripeRes);
       }
     }
   );
